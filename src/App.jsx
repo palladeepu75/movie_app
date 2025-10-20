65ca7f8dde99f890741ac26946c12d70efbbb5cb
Deepak <palladeepu75@gmail.com>
2025-10-20 16:41:43 +0530

    initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
 import './App.css'
+import Navbar from './components/Navbar'
 
 function App() {
 
 
   return (
     <>
+    <Navbar/>
      <h1>Movie App</h1>
     </>
   )
 }
 
 export default App
+
